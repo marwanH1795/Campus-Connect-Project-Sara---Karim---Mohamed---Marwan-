2cdf55a201d8f3bd5ef98337de972388d4e84b16
MarwanH1795 <marwan179205@aucegypt.edu>
2026-05-11 09:43:14 +0300

    Integrate final tests with app and server build

--- final_tests/CLIENT/MockNetworkClient.h
+++ final_tests/CLIENT/MockNetworkClient.h
@@ -1,12 +1,34 @@
-//We need to make this mock file in order to mock the call of connectToserver() function (we don't need a mock in the server side though)
+#ifndef MOCKNETWORKCLIENT_H
+#define MOCKNETWORKCLIENT_H
 
 #include "INetworkClient.h"
+
 #include <gmock/gmock.h>
+#include <QByteArray>
+#include <functional>
 
 class MockNetworkClient : public INetworkClient {
 public:
-    MOCK_METHOD(void, connectToServer, (const std::string& username), (override));
-    MOCK_METHOD(void, sendMessage, (const std::string& json), (override));
+    MOCK_METHOD(void, connectToServer,
+                (const std::string& username),
+                (override));
+
+    MOCK_METHOD(void, sendMessage,
+                (const std::string& json),
+                (override));
+
     MOCK_METHOD(void, setMessageHandler,
-        (std::function<void(const std::string&)> handler), (override));
+                (std::function<void(const std::string&)> handler),
+                (override));
+
+
+    MOCK_METHOD(void, sendBinary,
+                (const QByteArray& data),
+                (override));
+
+    MOCK_METHOD(void, setBinaryHandler,
+                (std::function<void(const QByteArray&)> handler),
+                (override));
 };
+
+#endif
--- final_tests/SERVER/ChatState.h
+++ final_tests/SERVER/ChatState.h
@@ -1,39 +1,6 @@
-#ifndef CHATSTATE_H
-#define CHATSTATE_H
+#ifndef FINAL_TESTS_SERVER_CHATSTATE_H
+#define FINAL_TESTS_SERVER_CHATSTATE_H
 
-#include <string>
-#include <vector>
-#include "Message.h"
-
-class ChatState {
-private:
-    std::string currentUser;
-    std::vector<std::string> onlineUsers;
-    std::vector<Message> publicMessages;
-
-    std::vector<std::string> joinedGroups;
-    std::vector<Message> groupMessages;
-
-public:
-    // user
-    void setCurrentUser(const std::string& username);
-    std::string getCurrentUser() const;
-
-    // online users
-    void setOnlineUsers(const std::vector<std::string>& users);
-    std::vector<std::string> getOnlineUsers() const;
-
-    // public chat
-    void addPublicMessage(const Message& message);
-    std::vector<Message> getPublicMessages() const;
-
-    // groups
-    void joinGroup(const std::string& groupId);
-    bool isInGroup(const std::string& groupId) const;
-    std::vector<std::string> getJoinedGroups() const;
-
-    void addGroupMessage(const Message& message);
-    std::vector<Message> getGroupMessagesForGroup(const std::string& groupId) const;
-};
+#include "../../src/logic/ChatState.h"
 
 #endif
